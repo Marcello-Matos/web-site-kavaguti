588457bc892e77410940b5c08c4b7918e6d01503
Marcello Matos <marcellomatosxads@gmail.com>
2024-07-09 12:42:26 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+
+var swiper = new Swiper(".team-slider",{
+    loop:true,
+    grabCursor:true,
+    spaceBetween:20,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+
+    }
+});
